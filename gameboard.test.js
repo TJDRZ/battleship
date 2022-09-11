7219641d8a443ebed26b63a340e62e2169f66046
TJ <tjdrzewiecki@gmail.com>
2022-09-11 14:28:22 -0400

    Added ship hit tests

--- gameboard.test.js
+++ gameboard.test.js
@@ -1,5 +1,5 @@
 const gameboard = require("./gameboard");
 
 test("add ship to gameboard", () => {
-    expect(gameboard.addShip(B, 2, V, length)).toEqual(); /* V means verticle, wherever you click will be the coordinates, then toggle between verticle and horizontal */
+    expect(gameboard.addShip(2, 2, V, length)).toEqual();
 });
--- ship.test.js
+++ ship.test.js
@@ -1,12 +1,21 @@
-const createShip = require("./ship")
+const createShip = require("./ship");
 
-test("creates ship's hp", () => {
-    expect(createShip(4).hp).toEqual([false, false, false, false]);
+test("creates ship's hitpoints", () => {
+  expect(createShip(4).hp).toEqual([false, false, false, false]);
 });
 
-test("ship gets sunk", () => {
-    const newShip = createShip(2);
+describe("ship gets hit", () => {
+  let newShip = {};
+  beforeEach(() => (newShip = createShip(2)));
+
+  test("proper hitpoint is hit", () => {
+    newShip.hit(1);
+    expect(newShip.hp).toEqual([false, true]);
+  });
+
+  test("ship gets sunk on all hitpoints hit", () => {
     newShip.hit(0);
     newShip.hit(1);
     expect(newShip.isSunk).toBeTruthy();
+  });
 });
